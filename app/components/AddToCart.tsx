0c5790981da9c1139f0868681a368a7fa1091619
Masoud <masoud.zeinalabedini@gmail.com>
2023-09-21 18:16:16 +0330

    Put users data into a table

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -4,7 +4,12 @@ import React from "react";
 const AddToCart = () => {
   return (
     <div>
-      <button onClick={() => console.log("Clicked!")}>Click Me!</button>
+      <button
+        className="btn btn-primary"
+        onClick={() => console.log("Clicked!")}
+      >
+        Click Me!
+      </button>
     </div>
   );
 };
