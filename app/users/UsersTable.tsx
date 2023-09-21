b51ee077054f35003297b349007e6b90494fa9e9
Masoud <masoud.zeinalabedini@gmail.com>
2023-09-21 18:23:52 +0330

    put users table into UsersTable component

--- app/users/UsersTable.tsx
+++ app/users/UsersTable.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
-const UsersTable = () => {
-  return (
-    <></>
-  )
+interface User {
+  id: number;
+  name: string;
+  email: string;
 }
 
-export default UsersTable
+const UsersTable = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+  const users: User[] = await res.json();
+  return (
+    <>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user: User) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default UsersTable;
