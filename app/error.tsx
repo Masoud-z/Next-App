e75bae94bc088fb761abfe3205f6741f12f7167c
Masoud <masoud.zeinalabedini@gmail.com>
2023-09-23 12:17:25 +0330

    Add retry btn to ErrorPage

--- app/error.tsx
+++ app/error.tsx
@@ -1,8 +1,20 @@
 "use client";
 import React from "react";
 
-const ErrorPage = () => {
-  return <div>An unexpected error has occurred.</div>;
+interface Props {
+  error: Error;
+  reset: () => void;
+}
+
+const ErrorPage = ({ error, reset }: Props) => {
+  return (
+    <>
+      <div>An unexpected error has occurred.</div>
+      <button className="btn" onClick={() => reset()}>
+        Retry
+      </button>
+    </>
+  );
 };
 
 export default ErrorPage;
