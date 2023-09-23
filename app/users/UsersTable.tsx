4190078b68b997597e9c1b134a073051602d47d2
Masoud <masoud.zeinalabedini@gmail.com>
2023-09-23 12:07:26 +0330

    Add ErrorPage

--- app/users/UsersTable.tsx
+++ app/users/UsersTable.tsx
@@ -13,7 +13,7 @@ interface Props {
 }
 
 const UsersTable = async ({ sortOrder }: Props) => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+  const res = await fetch("https://jsonplaceholder.typicode.com/user", {
     cache: "no-store",
   });
   const users: User[] = await res.json();
